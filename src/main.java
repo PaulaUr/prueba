b3f21abf96fe9bb058efa3bbce21972bd3d5065b
Paula A <paula.urquizo@gmail.com>
2016-09-21 12:43:19 +0200

    initial commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by pauli on 21/09/2016.
+ */
+public class main {
+
+    public static void main(String[] args){
+
+    }
+    void f(){
+        int a=1;
+        System.out.println("hola");
+        a=a+1;
+        System.out.println("hola: "+a);
+    }
+}
